dd3361f80a1b2e9eccfd7a018e807bca334c267f
Sanavi <sanavistanga@gmail.com>
2021-01-23 12:58:14 +0530

    Toady added a catapult

--- Slingshot.js
+++ Slingshot.js
@@ -6,6 +6,10 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
+        this.sling1image=loadImage("sprites/sling1.png");
+        this.sling2image=loadImage("sprites/sling2.png");
+        this.sling3image=loadImage("sprites/sling3.png");
+
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
@@ -16,12 +20,28 @@ class SlingShot{
     }
 
     display(){
+    image(this.sling1image,200,20);
+    image(this.sling2image,170,20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            push()
+            if(pointA.x<200){
+                strokeWeight(7);
+                stroke("blue");
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+26, pointB.y-4);
+                image(this.sling3image,pointA.x-33,pointA.y-10,15,30);
+            }
+        else{
+                strokeWeight(5);
+                stroke("blue");
+                line(pointA.x+20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+20, pointA.y, pointB.x+30, pointB.y-4);
+                image(this.sling3image,pointA.x+20,pointA.y-10,15,30);
+
+        }
+            pop()
         }
     }
-    
-}
+   }
